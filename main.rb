02900011a388df0d8ea6cdc2beadb39aed66404c
kenjisato0621 <ks01280719@gmail.com>
2020-03-06 03:11:53 -0300

    課題３提出

--- main.rb
+++ main.rb
@@ -5,19 +5,17 @@ brave = Brave.new(hp:300, offense:100, defense:50)
 monster = Monster.new(hp:200, offense:80, defense:30)
 
 
-brave.attack(monster) 
-   if monster.hp <= 0 
-   puts "モンスターを倒した。勇者の勝ちだ！"
+loop do
+ brave.attack(monster)
+ monster.attack(brave) 
+  
+   break if brave.hp <= 0 || monster.hp <= 0 
    end
-
-monster.attack(brave) 
-   if brave.hp <= 0 
-   puts "勇者は負けた。モンスターの勝ちだ！"
-   end
-
-
-
-
-
-
    
+   if brave.hp <= 0 
+    puts "勇者は負けた。モンスターの勝ちだ！"
+   end
+   
+   if monster.hp <= 0 
+    puts "モンスターを倒した。勇者の勝ちだ！"
+   end 
--- monster.rb
+++ monster.rb
@@ -2,13 +2,14 @@ require './character.rb'
 
 class Monster < Character
     
-    def attack(brave)
-     puts "モンスターの攻撃！"
-     damage = @offense - brave.defense
-     brave.hp -= damage
-     puts <<~text
-     勇者は#{damage}のダメ-ジを受けた！
-     勇者の残りの体力は#{brave.hp}だ！
-     text
-    end
+  def attack(brave)
+    puts "モンスターの攻撃！"
+    damage = @offense - brave.defense
+    brave.hp -= damage
+     
+    puts <<~text
+    勇者は#{damage}のダメージを受けた！
+    勇者の残りの体力は#{brave.hp}だ！
+    text
+  end
 end
